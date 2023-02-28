cb0dfddb9eb44e089716453353277d6173b1592c
linds0202 <lindsayarainey@gmail.com>
2023-02-28 10:02:42 -0800

    remove verification for testing

--- routes/resourcesRoutes.js
+++ routes/resourcesRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const resourcesController = require('../controllers/resourcesController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-router.use(verifyJWT)
+// router.use(verifyJWT)
 
 router.route('/')
     .get(resourcesController.getAllResources)
